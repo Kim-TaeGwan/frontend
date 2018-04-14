91dffa80a1f20c0b480105a24b2d207fa1255ccd
FULLNAM-PGNGJK9\Administrator <ab154@naver.com>
2018-04-14 18:00:07 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import axios from 'axios'; {/* axios연결 */}
 class App extends React.Component {
     
-    getCompany() {
+    constructor() {
+        super();
+        
+        this.state = {
+            title : '초기제목',
+            name : '초기이름'
+        }
+    }
+    
+ getCompany() { // 버튼을 누르면 실행
         //axios
         axios.get('http://localhost:4000/company') // 요청하면 response를 넘겨받음
             .then((response)=>{
                 console.log(response.data);
+                this.setState({
+                    title : response.data.title,
+                    name : response.data.name
+                })
             })
             .catch((error)=>{
                 console.log(error);
             });
-        
     }
     
 	render() {
@@ -20,6 +32,8 @@ class App extends React.Component {
                 <div>백엔드 통신 해보기</div>
                 <button onClick={this.getCompany.bind(this)}>
                     통신시작</button>
+                <div>{this.state.title}</div>
+                <div>{this.state.name}</div>
             </div>
 		)
 	}
---
+++ src/Application.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import axios from 'axios'; /* 통신모듈 axios연결 */
+
+import './Company.css';
+
+import Card from './Card';
+
+class Application extends React.Component {
+    
+    // 데이터 가져오기
+    constructor() {
+        super();
+        
+        this.state = {
+            count : 0,
+            company : []
+        }
+        
+        this.getList = this.getList.bind(this);
+        
+    }
+    
+    getList() {
+        axios.get('http://localhost:4000/company') // 요청하면 response를 넘겨받음
+            .then((response)=>{ //성공하면 then
+                console.log(response.data); // 콘솔로 response 잘 넘어오는지 확인
+                this.setState({
+                    count : response.data.count,
+                    company : response.data.company
+                })
+            })
+            .catch((error)=>{ // 실패하면 catch
+                console.log(error);
+            });
+    }
+    
+	render() {
+        
+        const {count, company} = this.state;
+        
+		return(
+            <div>    
+                <button onClick={this.getList}>데이터 가져오기</button>
+                <div>{count}</div> {/* 13으로 바뀔것 */}
+                
+                <hr style={{ margin : '10px 0' }} />
+                <div className="container">
+                    {company.map((value)=>{
+                        console.log(value.name)
+                        return <Card key={value.id} info={value} />
+                })} {/* map메소드는 뿌려줄수있음 중요! */}
+            </div>
+        </div>    
+            
+     
+		)
+	}
+}
+
+export default Application;
+
+
+
+
+
+
